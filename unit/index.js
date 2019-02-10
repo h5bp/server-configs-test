a8741383da3f5dcc7851e286e1d63afc92e9dec0
Léo Colombaro <git@colombaro.fr>
2019-02-10 18:29:47 +0100

    Add a run-all file

---
+++ unit/index.js
@@ -0,0 +1,66 @@
+import { group } from 'k6'
+import { options } from './abstract-test.js'
+import * as basicFileAccess from './basic-file-access.js'
+import * as cacheBusting from './cache-busting.js'
+import * as customErrors from './custom-errors.js'
+import * as forbiddenFiles from './forbidden-files.js'
+import * as precompressedFiles from './precompressed-files.js'
+import * as rewrites from './rewrites.js'
+import * as ssl from './ssl.js'
+
+export { options }
+
+export function setup () {
+  return {
+    basicFileAccess: {
+      name: "basic access",
+      setup: basicFileAccess.setup(),
+      // fn: basicFileAccess.default
+    },
+    cacheBusting: {
+      name: "cache busting",
+      setup: cacheBusting.setup(),
+      // fn: cacheBusting.default
+    },
+    customErrors: {
+      name: "custom errors",
+      setup: customErrors.setup(),
+      // fn: customErrors.default
+    },
+    forbiddenFiles: {
+      name: "forbidden files",
+      setup: forbiddenFiles.setup(),
+      // fn: forbiddenFiles.default
+    },
+    precompressedFiles: {
+      name: "precompressed files",
+      setup: precompressedFiles.setup(),
+      // fn: precompressedFiles.default
+    },
+    rewrites: {
+      name: "rewrites",
+      setup: rewrites.setup(),
+      // fn: rewrites.default
+    },
+    ssl: {
+      name: "ssl",
+      setup: ssl.setup(),
+      // fn: ssl.default
+    },
+  }
+}
+
+export default function (units) {
+  // for (const unit of units) {
+  //   group(unit.name, () => {
+  //     unit.fn(unit.setup)
+  //   })
+  // }
+  group(units.basicFileAccess.name, () => basicFileAccess.default(units.basicFileAccess.setup))
+  group(units.cacheBusting.name, () => cacheBusting.default(units.cacheBusting.setup))
+  group(units.customErrors.name, () => customErrors.default(units.customErrors.setup))
+  group(units.forbiddenFiles.name, () => forbiddenFiles.default(units.forbiddenFiles.setup))
+  group(units.precompressedFiles.name, () => precompressedFiles.default(units.precompressedFiles.setup))
+  group(units.rewrites.name, () => rewrites.default(units.rewrites.setup))
+  group(units.ssl.name, () => ssl.default(units.ssl.setup))
+}
