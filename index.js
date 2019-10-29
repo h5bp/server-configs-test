01cbf2b3772d30e76ccef49964113b1e85a75f98
Léo Colombaro <git@colombaro.fr>
2019-10-29 21:49:42 +0000

    GitHub Actions (#4)
    * Switch to GitHub Actions for CI
    * Make server-configs-test a GitHub Action

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+
+async function run () {
+  try {
+    console.log('Running k6 tests')
+    const ut = core.getInput('tests')
+    let args = ['run', 'lib/index.js']
+    if (Array.isArray(ut) && ut.length > 0) {
+      args += [
+        '-e',
+        `TESTS=${ut.join(':')}`
+      ]
+    }
+
+    await exec.exec('./bin/k6', args)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
