161ed2e095310849c00595446bac40ccfdafbdd8
Léo Colombaro <git@colombaro.fr>
2020-01-04 19:02:45 +0100

    Refactor GitHub Actions module

--- index.test.js
+++ index.test.js
@@ -1,11 +1,12 @@
 /* eslint-env jest */
 
-// const process = require('process')
+const process = require('process')
 const cp = require('child_process')
 const path = require('path')
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
+  process.env.INPUT_VERSION = 'true'
   const ip = path.join(__dirname, 'index.js')
-  console.log(cp.execSync(`node ${ip}`).toString())
+  expect(cp.execSync(`node ${ip}`).toString()).toEqual(expect.stringContaining('k6'))
 })
